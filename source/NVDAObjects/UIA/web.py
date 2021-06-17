236cbbd2a5a220efab4fcbda47fe5fa0aec155ed
Reef Turner <reef@nvaccess.org>
2021-06-17 16:15:17 +0800

    fixup! Rename UIA._getControlFieldForObject to _getControlFieldForUIAObject

--- source/NVDAObjects/UIA/web.py
+++ source/NVDAObjects/UIA/web.py
@@ -1,7 +1,7 @@
 # A part of NonVisual Desktop Access (NVDA)
 # This file is covered by the GNU General Public License.
 # See the file COPYING for more details.
-# Copyright (C) 2015-2020 NV Access Limited, Babbage B.V., Leonard de Ruijter
+# Copyright (C) 2015-2021 NV Access Limited, Babbage B.V., Leonard de Ruijter
 
 from comtypes import COMError
 from comtypes.automation import VARIANT
